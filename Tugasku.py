81d1149a325b45b0556a9dd6dc81ca3bf11496ff
Awalsyaputra101 <100498668+Awalsyaputra101@users.noreply.github.com>
2022-03-21 13:37:54 +0800

    Add files via upload

---
+++ Tugasku.py
@@ -0,0 +1,40 @@
+#type data list
+nama = ["jay","alim","awal","adri"]
+for nama in nama:
+    print(nama)
+
+#mengupdate
+nama =["jay","alim","awal""adri"]
+nama[1]="aldian"
+print(nama)
+
+#menambah
+nama = ["jay","alim","awal","adri"]
+nama.append("adri")
+print(nama)
+
+
+#menghapus 
+nama = ["jay","alim","awal","adri"]
+del nama [0]
+print(nama)
+
+#type data set
+se = {"adri","jay","alim","adri"}
+for se in se:
+    print(se)
+
+#mengupdet
+se = {"adri","jay","alim","adri"}
+#se[1]"andi"
+print(se)
+
+#menambahkan
+se = {"adri","jay","alim","adri"}
+se.add("awal syaputra")
+print(se)
+
+#menghapus
+se = {"adri","jay","alim","adri"}
+se.remove("adri")
+print(se)
